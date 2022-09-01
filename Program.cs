b60add159d57e63dec5f8320bfa3b4c69836e667
vgmda <vasilegancin@gmail.com>
2022-09-01 23:18:03 +0100

    Setting up the project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace GradeBook;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+
+        var result = numbers[0];
+        result = result + numbers[1];
+        result = result + numbers[2];
+        Console.WriteLine(result);
+
+    }
+}
