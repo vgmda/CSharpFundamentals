987081fee3998a3a6d5cfd17bbc1fae84df8eda5
vgmda <vasilegancin@gmail.com>
2022-09-02 22:05:15 +0100

    AddGrade()

--- Book.cs
+++ Book.cs
@@ -4,5 +4,11 @@ namespace GradeBook;
 
 class Book 
 {
+    List<double> grades;
+
+    public void AddGrade(double grade) 
+    {
+        grades.Add(grade);
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ class Program
     static void Main(string[] args)
     {
         var book = new Book();
+        book.AddGrade(89.1);
 
         // Explicit typing
+
         var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
         grades.Add(56.1);
 
