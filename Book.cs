37eb9543c7266dae51f41ce4d142f0adf1aab475
vgmda <vasilegancin@gmail.com>
2022-09-02 22:42:44 +0100

    Changes to Book()

--- Book.cs
+++ Book.cs
@@ -4,11 +4,45 @@ namespace GradeBook;
 
 class Book 
 {
-    List<double> grades;
+    public List<double> grades;
+    private string name;
 
+    // Explicit (implicit) constructor
+    public Book(string name)
+    {
+        grades = new List<double>();
+        this.name = name;
+
+    }
+
+    // Instance member (AddGrade) of a class book
     public void AddGrade(double grade) 
     {
         grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+        var result = 0.0;
+        var highGrade = double.MinValue;
+        var lowGrade = double.MaxValue;
+
+        foreach(double number in grades)
+        {
+            // if(number > highGrade)
+            // {
+            //     highGrade = number;
+            // }
+            highGrade = Math.Max(number, highGrade);
+            lowGrade = Math.Min(number, lowGrade);
+            result += number;
+        }
+        result /= grades.Count;
+
+        Console.WriteLine($"The lowest grade is {lowGrade}");
+        Console.WriteLine($"The highest grade is {highGrade}");
+        Console.WriteLine($"The average grade is {result:N1}");
+
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -7,22 +7,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        var book = new Book();
-        book.AddGrade(89.1);
+        var book = new Book("Grade Book");
+        book.AddGrade(4.1);
+        book.AddGrade(12.7);
+        book.AddGrade(55.2);
+        book.AddGrade(81.9);
+        book.grades.Add(95.3);
+        book.ShowStatistics();
+
 
         // Explicit typing
-
-        var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
-        grades.Add(56.1);
-
-        var result = 0.0;
-        foreach(double number in grades)
-        {
-            result += number;
-        }
-        result /= grades.Count;
-
-        Console.WriteLine($"The average grade is {result:N1}");
+        // var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+        // grades.Add(56.1);
 
     }
 }
