7fdcd184ecd7fe6e905908a3a493aff7334ce276
vgmda <vasilegancin@gmail.com>
2022-09-07 23:45:30 +0100

    Create test cases

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,6 +5,68 @@ namespace GradeBook.Tests;
 public class TypeTests
 {
     [Fact]
+    public void Test1()
+    {
+        // Given
+        var x = GetInt();
+        // When
+
+        // Then
+        Assert.Equal(3, x);
+    }
+
+    private object GetInt()
+    {
+        return 3;
+    }
+
+    [Fact]
+    public void CSharpCanPassByRef()
+    {
+        // Arrange
+        // var book1 = new Book("Book 1");
+        var book1 = GetBook("Book 1");
+        
+        
+        // Act
+        // SetName(book1, "New Name");
+        GetBookSetName(out book1, "New Name");
+
+        // Assert (Assertion)
+        Assert.Equal("New Name", book1.Name);
+
+    }
+
+    private void GetBookSetName(out Book book, string name)
+    {
+        book = new Book(name);
+    }
+
+
+    [Fact]
+    public void CSharpIsPassByValue()
+    {
+        // Arrange
+        // var book1 = new Book("Book 1");
+        var book1 = GetBook("Book 1");
+        
+        
+        // Act
+        // SetName(book1, "New Name");
+        GetBookSetName(book1, "New Name");
+
+        // Assert (Assertion)
+        Assert.Equal("Book 1", book1.Name);
+
+    }
+
+    private void GetBookSetName(Book book, string name)
+    {
+        book = new Book(name);
+    }
+
+
+    [Fact]
     public void CanSetNameFromReference()
     {
         // Arrange
